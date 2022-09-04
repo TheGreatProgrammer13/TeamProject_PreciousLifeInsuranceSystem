c9ae4091408d591c479fe46d11bfbc96ede846ea
Admin <Admin@DESKTOP-CACT1BI>
2022-09-05 00:26:23 +0800

    An Insurance Management System

---
+++ TeamProject_PreciousLifeInsuranceSystem/src/Precious_Life_Insurance_Management_System_Project/Precious_Life_Insurance_Management_System_Main.java
@@ -0,0 +1,10 @@
+package Precious_Life_Insurance_Management_System_Project;
+
+public class Precious_Life_Insurance_Management_System_Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
