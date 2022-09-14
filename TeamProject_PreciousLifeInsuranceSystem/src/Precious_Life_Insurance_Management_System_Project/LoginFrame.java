5783b7acac40c00673e42bf4c6f4d4730f0af0a2
xander <xander@DESKTOP-PA45VD5>
2022-09-14 23:43:11 +0800

    Login and Registration Update :)

--- TeamProject_PreciousLifeInsuranceSystem/src/Precious_Life_Insurance_Management_System_Project/LoginFrame.java
+++ TeamProject_PreciousLifeInsuranceSystem/src/Precious_Life_Insurance_Management_System_Project/LoginFrame.java
@@ -27,12 +27,19 @@ import javax.swing.SwingConstants;
 import javax.swing.JCheckBox;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import javax.swing.JButton;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.beans.VetoableChangeListener;
+import java.beans.PropertyChangeEvent;
 
 public class LoginFrame extends JFrame {
 	
 	
 
 	private JPanel contentPane;
+	private JTextField txtUsername;
+	private JPasswordField pwdPassword;
 
 	/**
 	 * Launch the application.
@@ -61,87 +68,113 @@ public class LoginFrame extends JFrame {
 		contentPane.setFont(new Font("Comic Sans MS", Font.BOLD, 14));
 		contentPane.setBounds(new Rectangle(0, 0, 20, 20));
 		contentPane.setBackground(Color.WHITE);
-		contentPane.setBorder(new LineBorder(Color.BLACK, 2));
+		contentPane.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
 		setContentPane(contentPane);
-		SpringLayout sl_contentPane = new SpringLayout();
-		contentPane.setLayout(sl_contentPane);
+		contentPane.setLayout(null);
 
 		JPanel panel = new JPanel();
-		sl_contentPane.putConstraint(SpringLayout.NORTH, panel, 0, SpringLayout.NORTH, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.WEST, panel, -15, SpringLayout.WEST, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.SOUTH, panel, 576, SpringLayout.NORTH, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.EAST, panel, -449, SpringLayout.EAST, contentPane);
+		panel.setBounds(-14, 1, 514, 576);
 		panel.setBackground(new Color(0, 45, 105));
 		contentPane.add(panel);
 
 		Button button = new Button("Sign In");
-		sl_contentPane.putConstraint(SpringLayout.NORTH, button, 409, SpringLayout.NORTH, contentPane);
+		button.setBounds(627, 400, 203, 40);
+		button.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				button.setForeground(new Color(189,151,87));
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent e) {
+				button.setForeground(new Color(205,196,125));
+			}
+			@Override
+			public void mousePressed(MouseEvent e) {
+				button.setForeground(new Color(146,102,44));
+			}
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				button.setForeground(new Color(205,196,125));
+			}
+		});
 		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		sl_contentPane.putConstraint(SpringLayout.WEST, button, 578, SpringLayout.WEST, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.EAST, button, -102, SpringLayout.EAST, contentPane);
 		button.setForeground(new Color(234, 218, 128));
 
 		button.setFont(new Font("Poppins", Font.BOLD, 20));
 		button.setBackground(new Color(0, 45, 105));
 		contentPane.add(button);
 
-		Button button_1 = new Button("Sign Up");
-		sl_contentPane.putConstraint(SpringLayout.SOUTH, button, -20, SpringLayout.NORTH, button_1);
-		sl_contentPane.putConstraint(SpringLayout.NORTH, button_1, 469, SpringLayout.NORTH, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.WEST, button_1, 578, SpringLayout.WEST, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.SOUTH, button_1, 513, SpringLayout.NORTH, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.EAST, button_1, 0, SpringLayout.EAST, button);
-		button_1.setForeground(new Color(234, 218, 128));
-		button_1.setFont(new Font("Poppins", Font.BOLD, 20));
-		button_1.setBackground(new Color(0, 45, 105));
+		Button button_1 = new Button("Create an account");
+		button_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Registration t = new Registration();
+				t.show();
+				dispose();
+
+			}
+		});
+		button_1.setBounds(627, 500, 203, 38);
+		button_1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				button_1.setForeground(new Color(189,151,87));
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent e) {
+				button_1.setForeground(new Color(205,196,125));
+			}
+			@Override
+			public void mousePressed(MouseEvent e) {
+				button_1.setForeground(new Color(146,102,44));
+			}
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				button_1.setForeground(new Color(205,196,125));
+			}
+		});
+		button_1.setForeground(Color.WHITE);
+		button_1.setFont(new Font("Dialog", Font.BOLD, 15));
+		button_1.setBackground(new Color(0, 128, 0));
 		contentPane.add(button_1);
 
 		JLabel lblNewLabel = new JLabel("Username:");
-		sl_contentPane.putConstraint(SpringLayout.WEST, lblNewLabel, 71, SpringLayout.EAST, panel);
+		lblNewLabel.setBounds(569, 191, 104, 26);
 		lblNewLabel.setFont(new Font("Poppins", Font.PLAIN, 20));
 		contentPane.add(lblNewLabel);
 
 		JLabel lblPassword = new JLabel("Password:");
-		sl_contentPane.putConstraint(SpringLayout.WEST, lblPassword, 0, SpringLayout.WEST, lblNewLabel);
+		lblPassword.setBounds(569, 286, 116, 26);
 		lblPassword.setFont(new Font("Poppins", Font.PLAIN, 20));
 		contentPane.add(lblPassword);
 
 		JLabel lblNewLabel_1 = new JLabel("Sign In");
-		sl_contentPane.putConstraint(SpringLayout.NORTH, lblNewLabel_1, 37, SpringLayout.NORTH, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.WEST, lblNewLabel_1, 182, SpringLayout.EAST, panel);
-		sl_contentPane.putConstraint(SpringLayout.EAST, lblNewLabel_1, -172, SpringLayout.EAST, contentPane);
+		lblNewLabel_1.setBounds(680, 38, 97, 35);
 		lblNewLabel_1.setFont(new Font("Poppins", Font.BOLD, 25));
 		contentPane.add(lblNewLabel_1);
 
 		JLabel lblNewLabel_2 = new JLabel("Precious Life Insurance");
-		sl_contentPane.putConstraint(SpringLayout.SOUTH, lblNewLabel_1, -38, SpringLayout.NORTH, lblNewLabel_2);
-		sl_contentPane.putConstraint(SpringLayout.WEST, lblNewLabel_2, 73, SpringLayout.EAST, panel);
+		lblNewLabel_2.setBounds(605, 111, 238, 29);
 		lblNewLabel_2.setFont(new Font("Dialog", Font.PLAIN, 22));
 		contentPane.add(lblNewLabel_2);
 
 		JSeparator separator = new JSeparator();
-		sl_contentPane.putConstraint(SpringLayout.NORTH, separator, 158, SpringLayout.NORTH, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.SOUTH, separator, -410, SpringLayout.SOUTH, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.NORTH, lblNewLabel, 22, SpringLayout.SOUTH, separator);
-		sl_contentPane.putConstraint(SpringLayout.WEST, separator, 73, SpringLayout.EAST, panel);
+		separator.setBorder(null);
+		separator.setBounds(571, 159, 305, 10);
 		panel.setLayout(null);
 		
 		JLabel lblIconLogo = new JLabel("");
 		lblIconLogo.setBounds(63, 65, 428, 427);
 		panel.add(lblIconLogo);
-		sl_contentPane.putConstraint(SpringLayout.NORTH, lblIconLogo, 39, SpringLayout.NORTH, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.WEST, lblIconLogo, -143, SpringLayout.EAST, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.SOUTH, lblIconLogo, 101, SpringLayout.NORTH, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.EAST, lblIconLogo, -35, SpringLayout.EAST, contentPane);
 		lblIconLogo.setIcon(new ImageIcon(LoginFrame.class.getResource("/images/logoPng.png")));
-		sl_contentPane.putConstraint(SpringLayout.EAST, separator, -73, SpringLayout.EAST, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.SOUTH, lblNewLabel_2, -19, SpringLayout.NORTH, separator);
 		contentPane.add(separator);
 
 		JLabel lblX = new JLabel("X");
+		lblX.setBounds(927, 1, 12, 28);
 		lblX.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -158,32 +191,53 @@ public class LoginFrame extends JFrame {
 				lblX.setForeground(Color.black);
 			}
 		});
-		sl_contentPane.putConstraint(SpringLayout.NORTH, lblX, 0, SpringLayout.NORTH, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.WEST, lblX, -22, SpringLayout.EAST, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.SOUTH, lblX, 28, SpringLayout.NORTH, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.EAST, lblX, -10, SpringLayout.EAST, contentPane);
 		lblX.setFont(new Font("Tahoma", Font.BOLD, 14));
 		lblX.setHorizontalAlignment(SwingConstants.CENTER);
 		contentPane.add(lblX);
 		
 		JPanel panel_1 = new JPanel();
-		sl_contentPane.putConstraint(SpringLayout.NORTH, panel_1, 10, SpringLayout.SOUTH, lblNewLabel);
-		sl_contentPane.putConstraint(SpringLayout.SOUTH, panel_1, -308, SpringLayout.SOUTH, contentPane);
+		panel_1.setBounds(571, 227, 305, 44);
 		panel_1.setBackground(Color.WHITE);
 		panel_1.setBorder(new LineBorder(new Color(128, 128, 128), 1, true));
-		sl_contentPane.putConstraint(SpringLayout.WEST, panel_1, 2, SpringLayout.WEST, lblNewLabel);
-		sl_contentPane.putConstraint(SpringLayout.EAST, panel_1, 0, SpringLayout.EAST, separator);
 		contentPane.add(panel_1);
 		
 		JPanel panel_1_1 = new JPanel();
-		sl_contentPane.putConstraint(SpringLayout.SOUTH, lblPassword, -12, SpringLayout.NORTH, panel_1_1);
-		sl_contentPane.putConstraint(SpringLayout.NORTH, panel_1_1, 323, SpringLayout.NORTH, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.SOUTH, panel_1_1, -42, SpringLayout.NORTH, button);
-		sl_contentPane.putConstraint(SpringLayout.WEST, panel_1_1, 0, SpringLayout.WEST, lblNewLabel);
-		sl_contentPane.putConstraint(SpringLayout.EAST, panel_1_1, 0, SpringLayout.EAST, separator);
+		panel_1_1.setBounds(569, 324, 307, 44);
 		panel_1_1.setBackground(Color.WHITE);
 		panel_1_1.setBorder(new LineBorder(new Color(128, 128, 128), 1, true));
 		panel_1.setLayout(null);
+		
+		txtUsername = new JTextField();
+		txtUsername.addVetoableChangeListener(new VetoableChangeListener() {
+			public void vetoableChange(PropertyChangeEvent evt) {
+			}
+		});
+		txtUsername.setForeground(Color.GRAY);
+		txtUsername.addFocusListener(new FocusAdapter() {
+		
+			@Override
+			public void focusGained(FocusEvent e) {
+				if(txtUsername.getText().equals("Username")) {
+					txtUsername.setText("");
+				}
+				else {
+					txtUsername.selectAll();
+				}
+			}
+			@Override
+			public void focusLost(FocusEvent e) {
+				if(txtUsername.getText().equals(""))
+					txtUsername.setText("Username");
+			}
+		});
+		txtUsername.setBorder(null);
+		txtUsername.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		txtUsername.setForeground(Color.GRAY);
+		txtUsername.setText("Username");
+		txtUsername.setBackground(Color.WHITE);
+		txtUsername.setBounds(51, 11, 228, 22);
+		panel_1.add(txtUsername);
+		txtUsername.setColumns(10);
 		
 		JLabel lblUsernameIcon = new JLabel("");
 		lblUsernameIcon.setHorizontalAlignment(SwingConstants.CENTER);
@@ -202,6 +256,44 @@ public class LoginFrame extends JFrame {
 		lblNewLabel_4.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_4.setIcon(new ImageIcon(LoginFrame.class.getResource("/images/PassLogo.png")));
 		panel_1_1.add(lblNewLabel_4);
+		
+		pwdPassword = new JPasswordField();
+		sl_panel_1_1.putConstraint(SpringLayout.NORTH, pwdPassword, 10, SpringLayout.NORTH, panel_1_1);
+		sl_panel_1_1.putConstraint(SpringLayout.WEST, pwdPassword, 5, SpringLayout.EAST, lblNewLabel_4);
+		sl_panel_1_1.putConstraint(SpringLayout.SOUTH, pwdPassword, -7, SpringLayout.SOUTH, lblNewLabel_4);
+		sl_panel_1_1.putConstraint(SpringLayout.EAST, pwdPassword, 241, SpringLayout.EAST, lblNewLabel_4);
+		pwdPassword.setFont(new Font("Poppins", Font.PLAIN, 15));
+		pwdPassword.setForeground(Color.GRAY);
+		pwdPassword.setEchoChar((char)0);
+		pwdPassword.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				if(pwdPassword.getText().equals("Password")) {
+					pwdPassword.setEchoChar('●');
+					pwdPassword.setText("");
+					
+				}
+				else {
+					pwdPassword.selectAll();
+				}
+			}
+			@Override
+			public void focusLost(FocusEvent e) {
+				if(pwdPassword.getText().equals("")) {
+					pwdPassword.setText("Password");
+					pwdPassword.setEchoChar((char)0);
+					
+				}
+			}
+		});
+		pwdPassword.setText("Password");
+		pwdPassword.setBorder(null);
+		panel_1_1.add(pwdPassword);
+		
+		JSeparator separator_1 = new JSeparator();
+		separator_1.setBorder(null);
+		separator_1.setBounds(569, 470, 317, 10);
+		contentPane.add(separator_1);
 		
 		
 	}
